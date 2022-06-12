4e50ec78735be0d2e220d94846ce507c14d2d0d6
Ajay <ajay.ip3105@gmail.com>
2022-06-12 11:24:10 -0400

    Completed assignment

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
+
+function QuestionList({setQuestions, questions}) {
+  useEffect(() => {
+    fetch('http://localhost:4000/questions')
+    .then (r => r.json())
+    .then(data => setQuestions(data))
+  },[setQuestions])
+
+  function handleDelete (deleteQuestion){
+    const updatedQuestion = questions.filter(question => question.id !== deleteQuestion.id)
+    setQuestions(updatedQuestion)
+  }
+
+  function handleUpdate(updatedQuestion){
+    const updatedQuestions = questions.map(question =>{
+      if(question.id === updatedQuestion.id){
+        return updatedQuestion
+      }else{
+        return question
+      }
+    })
+    setQuestions(updatedQuestions)
+  }
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questions.map(question =><QuestionItem key={question.id}
+          question ={question} onDelete={handleDelete}
+          onUpdate = {handleUpdate}/>)
+        }
+      </ul>
     </section>
   );
 }
